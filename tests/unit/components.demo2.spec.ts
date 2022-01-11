ef6e84a2ff447afe5334b969b7aada3dfa03333b
Juching <azhucheng1@qq.com>
2022-01-11 17:01:21 +0800

    test: 一个测试文件多个测试用例

--- tests/unit/components.demo2.spec.ts
+++ tests/unit/components.demo2.spec.ts
@@ -3,13 +3,20 @@ import CompositionApi from '/@/components/Demo/CompositionApi.vue';
 // console.log(`🚀 ~ CompositionApi`, CompositionApi);
 
 describe('CompositionApi.vue', () => {
-  it('props.message toUpperCase', () => {
-    const message = 'Testing the composition API';
-    const wrapper = shallowMount(CompositionApi, {
-      props: { message },
-    });
+  const message = 'Testing the composition API';
+  const wrapper = shallowMount(CompositionApi, {
+    props: { message },
+  });
 
+  it('props.message toUpperCase', () => {
     // expect(wrapper.text()).toMatch(message);
     expect(wrapper.find('.message').text()).toBe('TESTING THE COMPOSITION API');
+  });
+
+  it('data.count add 1', async () => {
+    wrapper.find('button').trigger('click');
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.find('.count').text()).toBe('Count: 1');
   });
 });
