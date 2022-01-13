0bd916ba6ca167aa1949f41ee39e3467f79b58be
Juching <azhucheng1@qq.com>
2022-01-13 18:12:26 +0800

    wip: 全局路由守卫 & 组件内路由钩子

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -23,4 +23,32 @@ const router = createRouter({
   routes, // `routes: routes` 的缩写
 });
 
+/**
+ * [完整的导航解析流程]
+ * 导航被触发。
+ * 在失活的 `组件里` 调用 beforeRouteLeave 守卫。
+ * 调用 `全局` 的 beforeEach 守卫。
+ * 在重用的 `组件里` 调用 beforeRouteUpdate 守卫(2.2+)。
+ * 在路由配置里调用 beforeEnter。
+ * 解析异步路由组件。
+ * 在被激活的 `组件里` 调用 beforeRouteEnter。
+ * 调用 `全局` 的 beforeResolve 守卫(2.5+)。
+ * 导航被确认。
+ * 调用 `全局` 的 afterEach 钩子。
+ * 触发 DOM 更新。
+ * 调用 beforeRouteEnter 守卫中传给 next 的回调函数，创建好的组件实例会作为回调函数的参数传入。
+ */
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  // console.log(`🚀 ~ beforeEach ~ from`, from);
+  // console.log(`🚀 ~ beforeEach ~ to`, to);
+  next();
+});
+// 全局后置钩子
+router.afterEach(() => {
+  // console.log(`🚀 ~ afterEach ~ from`, from);
+  // console.log(`🚀 ~ afterEach ~ to`, to);
+});
+
 export { routes, router };
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,4 +4,17 @@
 
 <script setup lang="ts">
   import { HelloWorld } from '/@/components/Demo';
+  import { onBeforeRouteLeave } from 'vue-router';
+
+  // 组件内的守卫
+  // beforeRouteEnter
+  // beforeRouteUpdate
+  // beforeRouteLeave
+  onBeforeRouteLeave((to, from) => {
+    console.log(`🚀 ~ onBeforeRouteLeave ~ from`, from);
+    console.log(`🚀 ~ onBeforeRouteLeave ~ to`, to);
+    const answer = window.confirm('Do you really want to leave? you have unsaved changes!');
+    // 取消导航并停留在同一页面上
+    if (!answer) return false;
+  });
 </script>
