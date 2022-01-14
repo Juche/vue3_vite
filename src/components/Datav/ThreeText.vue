47815a2e7915b793eeb093c851db3885be639cda
Juching <azhucheng1@qq.com>
2022-01-14 18:46:55 +0800

    wip: add an empty component

---
+++ src/components/Datav/ThreeText.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="info">Description</div>
+</template>
+
+<script lang="ts" setup>
+  import * as THREE from 'three';
+  import { onMounted } from 'vue';
+
+  const width = 200;
+  const height = 100;
+
+  function init() {}
+
+  // init();
+  onMounted(() => {
+    init();
+  });
+</script>
+
+<style lang="less" scoped>
+  #info {
+    position: absolute;
+    top: 10px;
+    width: 100%;
+    text-align: center;
+    z-index: 100;
+    display: block;
+  }
+</style>
