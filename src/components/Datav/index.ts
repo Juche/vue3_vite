99adc30f066d2a2f0ed1a744d9054d886d3c4487
Juching <azhucheng1@qq.com>
2022-01-14 14:48:25 +0800

    wip: 添加一个 threejs 小狮子示例

--- src/components/Datav/index.ts
+++ src/components/Datav/index.ts
@@ -1,5 +1,6 @@
 import Three01 from './Three01.vue';
 import Three02 from './Three02.vue';
+import Three03 from './Three03.vue';
 import Cesium01 from './Cesium01.vue';
 
-export { Three01, Three02, Cesium01 };
+export { Three01, Three02, Three03, Cesium01 };
--- src/views/datav/three.vue
+++ src/views/datav/three.vue
@@ -1,8 +1,9 @@
 <template>
-  <Three01 />
-  <Three02 />
+  <!-- <Three01 />
+  <Three02 /> -->
+  <Three03 />
 </template>
 
 <script setup lang="ts">
-  import { Three01, Three02 } from '/@/components/Datav';
+  import { Three01, Three02, Three03 } from '/@/components/Datav';
 </script>
