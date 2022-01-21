8660023aa822d48feeb6845fc795118fdad4b9e3
Juching <azhucheng1@qq.com>
2022-01-21 11:58:55 +0800

    wip: store 跨页面示例

--- src/views/demo/index.vue
+++ src/views/demo/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <h3>demo</h3>
+  <h1>demo</h1>
+  <h3>{{ message.message }}</h3>
 </template>
 
 <script setup lang="ts">
+  import { useMessageStore } from '/@/stores/message';
+  const message = useMessageStore();
+
   let person1: Person = {
     name: 'Juche',
     gender: 'female',
