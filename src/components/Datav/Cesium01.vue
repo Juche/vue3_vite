1ef0a91850bce267c22b37f943c24f2840cb97cb
Juching <azhucheng1@qq.com>
2022-02-08 18:44:43 +0800

    wip: cesium viewer

--- src/components/Datav/Cesium01.vue
+++ src/components/Datav/Cesium01.vue
@@ -7,24 +7,54 @@
   import * as Cesium from 'cesium';
 
   onMounted(() => {
-    const viewer = new Cesium.Viewer('cesiumContainer');
-    var wyoming = viewer.entities.add({
-      name: 'Wyoming',
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+      imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
+        url: '//services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer',
+      }),
+      baseLayerPicker: false,
+    });
+
+    const layers = viewer.scene.imageryLayers;
+    const blackMarble = layers.addImageryProvider(
+      new Cesium.TileMapServiceImageryProvider({
+        url: '//cesiumjs.org/tilesets/imagery/blackmarble',
+        maximumLevel: 8,
+        credit: 'Black Marble imagery courtesy NASA Earth Observatory',
+      }),
+    );
+
+    const wuhan = viewer.entities.add({
+      name: 'Wuhan',
       polygon: {
+        // hierarchy: Cesium.Cartesian3.fromDegreesArray([105, 35, 105, 25, 115, 25, 115, 35]),
         hierarchy: Cesium.Cartesian3.fromDegreesArray([
-          -109.080842, 45.002073, -105.91517, 45.002073, -104.058488, 44.996596, -104.053011,
-          43.002989, -104.053011, 41.003906, -105.728954, 40.998429, -107.919731, 41.003906,
-          -109.04798, 40.998429, -111.047063, 40.998429, -111.047063, 42.000709, -111.047063,
-          44.476286, -111.05254, 45.002073,
+          114.39, 30.49, 114.41, 30.49, 114.41, 30.51, 114.39, 30.51,
         ]),
-        height: 0,
-        material: Cesium.Color.RED.withAlpha(0.2),
+        height: 2000,
+        extrudedHeight: 2400,
+        material: Cesium.Color.RED.withAlpha(0.5),
         outline: true,
         outlineColor: Cesium.Color.GREEN,
       },
     });
 
-    viewer.zoomTo(wyoming);
+    const entity = viewer.entities.add({
+      polyline: {
+        positions: Cesium.Cartesian3.fromDegreesArray([114, 30.5, 115, 30.5]),
+        width: 10,
+        material: Cesium.Color.RED,
+      },
+    });
+
+    const polyline = entity.polyline; // For upcoming examples
+
+    polyline.material = new Cesium.PolylineGlowMaterialProperty({
+      glowPower: 0.2,
+      color: Cesium.Color.BLUE,
+    });
+
+    viewer.zoomTo(wuhan);
+    // viewer.zoomTo(viewer.entities);
   });
 </script>
 
