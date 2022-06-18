52d0113c6da7daf1d04b4ead700a69dbc763286a
zhucheng <zhucheng@agree.com.cn>
2022-06-18 21:51:37 +0800

    feat: threejs 初始化封装类接收页面 canvas 节点

--- src/components/Datav/MultiBox.vue
+++ src/components/Datav/MultiBox.vue
@@ -1,7 +1,18 @@
 <template>
   <h1>Threejs Demo</h1>
+  <canvas id="canvasDom" ref="canvasDom"></canvas>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { onMounted, ref } from 'vue';
+  import { $T } from '/@/plugins/threejs';
+
+  const canvasDom = ref(null);
+
+  onMounted(() => {
+    const T = new $T(canvasDom.value!);
+    // T.init();
+  });
+</script>
 
 <style lang="less" scoped></style>
---
+++ src/plugins/threejs/index.ts
@@ -0,0 +1,7 @@
+export class $T {
+  private dom: HTMLElement;
+  constructor(dom: HTMLElement) {
+    this.dom = dom;
+    console.log(`🚀 ~ $T dom`, dom);
+  }
+}
