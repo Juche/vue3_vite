4f1e49bb42c1dab513687af289e6d4756b013782
zhucheng <zhucheng@agree.com.cn>
2022-06-18 22:06:56 +0800

    wip: update

--- src/components/Datav/MultiBox.vue
+++ src/components/Datav/MultiBox.vue
@@ -1,16 +1,15 @@
 <template>
-  <h1>Threejs Demo</h1>
-  <canvas id="canvasDom" ref="canvasDom"></canvas>
+  <div id="threeDom" ref="threeDom"></div>
 </template>
 
 <script lang="ts" setup>
   import { onMounted, ref } from 'vue';
   import { $T } from '/@/plugins/threejs';
 
-  const canvasDom = ref(null);
+  const threeDom = ref(null);
 
   onMounted(() => {
-    const T = new $T(canvasDom.value!);
+    const T = new $T(threeDom.value!);
     // T.init();
   });
 </script>
--- src/plugins/threejs/index.ts
+++ src/plugins/threejs/index.ts
@@ -1,7 +1,14 @@
+import { WebGLRenderer } from 'three';
+
 export class $T {
   private dom: HTMLElement;
+  private renderer: WebGLRenderer;
   constructor(dom: HTMLElement) {
     this.dom = dom;
+    this.renderer = new WebGLRenderer();
+    dom.appendChild(this.renderer.domElement);
+    this.renderer.setSize(dom.offsetWidth, dom.offsetHeight);
+    // this.renderer.render();
     console.log(`🚀 ~ $T dom`, dom);
   }
 }
